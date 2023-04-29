d8110f57c4a17df44f14fdf7b828b42a2bef8e11
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-04-29 11:32:20 +0300

    add new end point to JokeService

--- app/src/main/java/com/fighter/joketoday/data/networking/JokeService.kt
+++ app/src/main/java/com/fighter/joketoday/data/networking/JokeService.kt
@@ -1,4 +1,10 @@
 package com.fighter.joketoday.data.networking
 
+import com.fighter.joketoday.data.model.JokeResponse
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+
 interface JokeService {
+    @GET("joke/Any")
+    fun getRandomJoke(): Single<JokeResponse>
 }
