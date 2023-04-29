a543ed3d3361b16e6ea533850e05a4c8804610a0
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-04-29 11:36:45 +0300

    implement retrofit api builder pattern

--- app/src/main/java/com/fighter/joketoday/data/networking/API.kt
+++ app/src/main/java/com/fighter/joketoday/data/networking/API.kt
@@ -1,4 +1,14 @@
 package com.fighter.joketoday.data.networking
 
+import com.fighter.joketoday.utils.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object API {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val jokeService: JokeService = retrofit.create(JokeService::class.java)
 }
