a47c6792c86efb9d940eec213120b8c59dc35089
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-04-29 11:57:37 +0300

    initiate MainViewModel

--- app/src/main/java/com/fighter/joketoday/data/JokeRepository.kt
+++ app/src/main/java/com/fighter/joketoday/data/JokeRepository.kt
@@ -1,4 +1,8 @@
 package com.fighter.joketoday.data
 
+import com.fighter.joketoday.data.networking.API
+
 class JokeRepository {
+    fun getRandomJoke() = API.jokeService.getRandomJoke()
+
 }
--- app/src/main/java/com/fighter/joketoday/ui/MainViewModel.kt
+++ app/src/main/java/com/fighter/joketoday/ui/MainViewModel.kt
@@ -1,6 +1,39 @@
 package com.fighter.joketoday.ui
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.fighter.joketoday.data.JokeRepository
+import com.fighter.joketoday.data.model.JokeResponse
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainViewModel : ViewModel() {
+    private val TAG = "MainViewModel"
+    private val compositeDisposable = CompositeDisposable()
+    private val repository = JokeRepository()
+    private val _randomJoke = MutableLiveData<JokeResponse>()
+    val randomJoke: LiveData<JokeResponse>
+        get() = _randomJoke
+
+    init {
+        getRandomJoke()
+    }
+    private fun getRandomJoke() {
+        val randomJoke = repository.getRandomJoke()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::onGetRandomJoke , this::onError)
+        compositeDisposable.add(randomJoke)
+    }
+
+    private fun onGetRandomJoke(jokeResponse: JokeResponse){
+        Log.e(TAG ,"${jokeResponse.setup}" )
+        _randomJoke.postValue(jokeResponse)
+    }
+    private fun onError(e:Throwable){
+        Log.e(TAG ,"${e.message}" )
+    }
 }
